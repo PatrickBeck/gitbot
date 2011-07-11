dfb092206b3627cb0b756ac25c86550af761e18a
Patrick Beck <pbeck@yourse.de>
2011-07-11 18:15:36 +0200

    Now runtest.sh is working ;)

--- runtest.sh
+++ runtest.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 # Prüft ob gitbot läuft.
-if [ $(ps aux | grep -c 'python gitbot.py') = 0 ];
-then
-    echo "$(date) gitbot abgestürzt, neu starten"
-    python gitbot.py
-fi
+pgrep -fl gitbot.py > /dev/null 2>&1 || python /home/pbeck/gitbot/gitbot.py
 
