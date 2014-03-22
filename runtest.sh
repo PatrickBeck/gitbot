4f368e29798502457ef215622a3f71694880eaec
Patrick Beck <pbeck@yourse.de>
2014-03-22 15:56:10 +0100

    runtest not working without -f parameter

--- runtest.sh
+++ runtest.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Prüft ob gitbot läuft.
-pgrep -l gitbot.py > /dev/null 2>&1 || python /home/pbeck/gitbot/gitbot.py
+pgrep -fl gitbot.py > /dev/null 2>&1 || python /home/pbeck/gitbot/gitbot.py
 
